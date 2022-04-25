730ee12030f8585f88f363ff2e9084a625c51214
Kawabongo <98980709+real-kunnun@users.noreply.github.com>
2022-04-25 16:51:33 +0200

    exercise 3 plus show in DOM

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-function changeH1() {
-    document.querySelector("h1").innerHTML = "Edited with JS"
-}
+/*Send to :
+schadi.faour@powercoders.org
+ammarelseed@icloud.com
+hossameldin.baioumy@powercoders.org
+*/
+
 
 //Exercise 1
 function calc() {
     let n1 = document.getElementById("firstN").value
     let n2 = document.getElementById("secondN").value
     if (n1 > n2) {
+        document.getElementById("result").innerHTML = "The greater number of " + n1 + " and " + n2 + " is " + n1;
         console.log("The greater number of " + n1 + " and " + n2 + " is " + n1)
-        document.createElement("p").innerHTML = "The smaller number of" + n1 + "and" + n2 + "is" + n1
         return n1, n2
     } else {
+        document.getElementById("result").innerHTML = "The smaller number of " + n1 + " and " + n2 + " is " + n1;
         console.log("The smaller number of " + n1 + " and " + n2 + " is " + n1)
-        document.createElement("p").innerHTML = "The smaller number of" + n1 + "and" + n2 + "is" + n1
         return n2, n1
     }
+}
+
+//Exercise 2
+function helloWorld(){
+    let langCode = document.getElementById("lCode").value
+    if (langCode === "es"){
+        console.log("Hola Mundo")
+    } else if (langCode === "de"){
+        console.log("Hallo Welt")
+    } else if (langCode === "en"){
+        console.log("Hello World")
+    } else {
+        console.log("Not a valid language code")
+    }
+}
+
+//Exercise 3
+function retirement(){
+    let userAge = document.getElementById("userAge").value
+    let retAge = document.getElementById("retAge").value
+
+     alert("You have " + (retAge - userAge) + " years left until you can retire." + "It's " + new Date().getFullYear() + ", so you can retire in " + (new Date().getFullYear() + (retAge - userAge)) + ".")
+
+
+    /*let age = document.getElementById("age").value
+    let retirementAge = document.getElementById("retirementAge").value
+    let currentAge = 2020 - age
+    let retirementYear = currentAge + retirementAge
+    console.log("You will retire in " + retirementYear)
+    document.createElement("p").innerHTML = "You will retire in " + retirementYear*/
 }
 
 //The greater number of 5 and 10 is 10.
